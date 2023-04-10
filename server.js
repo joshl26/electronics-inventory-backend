3cd181819034b962821ee094a29849e95fab2b34
Joshua Lehman <joshlehman.dev@gmail.com>
2023-04-09 23:07:50 -0400

    commit

--- server.js
+++ server.js
@@ -29,9 +29,10 @@ app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
 app.use("/auth", require("./routes/authRoutes"));
-app.use("/notes", require("./routes/noteRoutes"));
-app.use("/parts", require("./routes/partRoutes"));
+
 app.use("/users", require("./routes/userRoutes"));
+app.use("/parts", require("./routes/partRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
