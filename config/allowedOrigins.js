77b3fcf81b6199a56dfcb9bfa758dfaabb3445d6
Joshua Lehman <joshlehman.dev@gmail.com>
2023-04-13 00:33:34 -0400

    add el-in.ca as allowed origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://web.postman.co/",
   "https://electronics-inventory-client.onrender.com",
+  "https://el-in.ca",
 ];
 
 module.exports = allowedOrigins;
