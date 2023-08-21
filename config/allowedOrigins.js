27f557f00f5ddf61dc2c7bb3d9f6866e4f538b0d
Joshua Lehman <joshlehman.dev@gmail.com>
2023-08-21 17:46:29 -0400

    updates

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,8 @@
 const allowedOrigins = [
-  // "http://localhost:3000",
+  "http://localhost:3000",
   // "https://web.postman.co/",
-  "https://electronics-inventory-client.onrender.com",
-  "https://www.el-in.ca",
+  // "https://electronics-inventory-client.onrender.com",
+  // "https://www.el-in.ca",
 ];
 
 module.exports = allowedOrigins;
