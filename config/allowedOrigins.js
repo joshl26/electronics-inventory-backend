73cfaf5a4325a0a89f6ce877a9c65a03091d0ed6
Joshua Lehman <joshlehman.dev@gmail.com>
2023-09-03 09:59:37 -0400

    update-allowed-origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,9 @@
-//TODO select based on production vs dev
 const allowedOrigins = [
-  "http://localhost:3000",
+  process.env.NODE_ENV === "production"
+    ? "https://electronics-inventory-client.onrender.com"
+    : "http://localhost:3000",
+
   // "https://web.postman.co/",
-  // "https://electronics-inventory-client.onrender.com",
   // "https://www.el-in.ca",
 ];
 
