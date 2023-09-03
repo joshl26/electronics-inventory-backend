a46fb92f6e5836ef6af7201ed1895debb9ad10de
Joshua Lehman <joshlehman.dev@gmail.com>
2023-09-03 10:09:09 -0400

    allowed-origin-updates

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,8 +2,7 @@ const allowedOrigins = [
   process.env.NODE_ENV === "production"
     ? "https://www.el-in.ca"
     : "http://localhost:3000",
-
-  // "https://web.postman.co/",
+  "https://web.postman.co/",
 ];
 
 module.exports = allowedOrigins;
