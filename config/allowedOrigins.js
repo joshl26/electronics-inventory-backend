e07adf6e819d191a508dd3a44cc34ca87a62afa4
joshl26 <joshl26@hotmail.com>
2025-11-20 10:15:53 -0500

    feat: updates

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
-  process.env.NODE_ENV === 'production' ? 'https://electronics-inventory-client.onrender.com/' : 'http://localhost:3000',
-  'https://web.postman.co/',
+  process.env.NODE_ENV === 'production' ? 'https://electronics-inventory-client.onrender.com' : 'http://localhost:3000',
+  'https://web.postman.co',
 ];
 
 module.exports = allowedOrigins;
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -5,6 +5,7 @@ const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
   origin: (origin, callback) => {
+    console.log('CORS origin:', origin);
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
